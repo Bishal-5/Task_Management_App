6828f991bf5ff82ad3b8438d4309b24bfeeccf8b
Bishal-5 <naskarbishal514@gmail.com>
2025-08-23 10:44:13 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(
   cors({
-    origin: 'https://task-management-app-client-eight.vercel.app/' || process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: 'https://task-management-app-client-eight.vercel.app' || process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
